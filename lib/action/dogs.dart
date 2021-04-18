4ac64fc01affa7e728d18426e7ed9babc738a747
warinthon30 <jaitrong3016@gmail.com>
2021-04-18 21:51:09 +0700

    commit2

---
+++ lib/action/dogs.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ActionDog {
+  String collection_name = 'DataDog';
+  FirebaseFirestore firestore_db;
+
+  ActionDog() {
+    firestore_db = FirebaseFirestore.instance;
+  }
+
+  getAllDog() async {
+    final collection = firestore_db.collection(collection_name);
+    QuerySnapshot res = await collection.get();
+    final dataList = res.docs.map((doc) => doc.data()).toList();
+    print(dataList);
+    return dataList;
+  }
+
+  addNewDog(
+    Map<String, dynamic> data,
+  ) async {
+    final collection = firestore_db.collection(collection_name);
+    collection.doc(data['name']).set(data);
+  }
+
+  updateDog() async {}
+
+  deleteDog(String docName) async {
+    final collection = firestore_db.collection(collection_name);
+    collection.doc(docName).delete();
+  }
+}
